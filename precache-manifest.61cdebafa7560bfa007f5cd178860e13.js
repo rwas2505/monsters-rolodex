50062332eb3928f5296959bbf093875cdc948d10
Ryan <ryanwaszak@gmail.com>
2020-10-13 10:59:32 -0500

    Updates

---
+++ precache-manifest.61cdebafa7560bfa007f5cd178860e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4bbac8916465e1912f787f0e22e40d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2843d1029aed709d7d3c",
+    "url": "/monsters-rolodex/static/css/main.ece91cdf.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2843d1029aed709d7d3c",
+    "url": "/monsters-rolodex/static/js/main.343cde4f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
